b553c8bc44214877f744a9a4d790e676a3f88a7b
khushalSonagara <khushalsonagara77@gmail.com>
2025-05-27 08:28:38 +0530

    vite onfiguration update

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   optimizeDeps: {
     include: ["pdfjs-dist"],
   },
   build: {
+    outDir: "dist",
     commonjsOptions: {
       include: [/node_modules/],
     },
